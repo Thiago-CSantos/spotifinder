c3944be3138c18913fcd7d35cbdcfc8f06e92391
Thiago Santos <thiagocarretero433@gmail.com>
2025-04-29 14:57:49 -0300

    feat: arrumando todos os imports corretamente e a estrutura de pastas do tabs

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet } from 'react-native';
+import EditScreenInfo from '@/src/common/components/EditScreenInfo';
+import { Text, View } from '@/src/common/components/Themed';
+import React from 'react';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabTwoScreen() {
+export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
